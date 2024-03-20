ab985884bd9674857f722d557b6283cde989d88d
srikishore5727 <srikishore9080676683@gmail.com>
2024-03-20 17:28:13 +0530

    created navbar with responsive view used tailwindcss

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Components/footer/Footer";
+import Home from "./Components/home/Home";
+import Navbar from "./Components/navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/> 
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
