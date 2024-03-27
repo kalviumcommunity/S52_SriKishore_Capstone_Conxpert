e018162b606e88b44e2f2f53723bd0bc97ac166b
srikishore5727 <srikishore9080676683@gmail.com>
2024-03-27 15:51:23 +0530

    created a mentors card and user review card with responsive

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -4,6 +4,15 @@ import findMentor from '../../public/assets/kickstart-images/findMentor.png'
 import interviewPay from '../../public/assets/kickstart-images/interview-pay.png'
 import BookSession from '../../public/assets/kickstart-images/bookSession.png'
 import BulletIcon from '../../public/assets/kickstart-images/bulletIcon.png'
+import mentor1 from '../../public/assets/Mentors-images/mentor1.jpeg'
+import mentor2 from '../../public/assets/Mentors-images/mentor2.jpg'
+import mentor3 from '../../public/assets/Mentors-images/mentor3.jpg'
+import userPic1 from '../../public/assets/users-pic/user-pic1.jpeg'
+import userPic2 from '../../public/assets/users-pic/user-pic2.jpg'
+
+
+
+
 import { BsArrowRight } from 'react-icons/bs'
 import HeaderTwo from '../Components/Header/HeaderTwo'
 
@@ -96,7 +105,7 @@ const Home = () => {
                 <h2 className='text-[26px] leading-9 text-white font-[700] text-center'>Payment</h2>
 
                 <p className='text-[16px] leading-7 text-[#cfcfcf] font-[400] mt-4 text-center'>
-                Join interviews with ease on ConXpert! Pay a low cost and open doors to your future career journey.
+                  Join interviews with ease on ConXpert! Pay a low cost and open doors to your future career journey.
                 </p>
 
                 <Link
@@ -115,7 +124,7 @@ const Home = () => {
                 <h2 className='text-[26px] leading-9 text-white font-[700] text-center'>Book Session</h2>
 
                 <p className='text-[16px] leading-7 text-[#cfcfcf] font-[400] mt-4 text-center'>
-                Ready to take the next step? Book your session now and embark on your journey to success with ConXpert!
+                  Ready to take the next step? Book your session now and embark on your journey to success with ConXpert!
                 </p>
 
                 <Link
@@ -131,7 +140,7 @@ const Home = () => {
       </section>
 
       {/* ====== second navbar ====== */}
-      <HeaderTwo/>
+      <HeaderTwo />
 
 
       {/* ====== detail section ===== */}
@@ -141,41 +150,95 @@ const Home = () => {
 
         {/* ====== plan section ====== */}
         <div>
-        <div className='text-[28px] mt-[100px] text-center'>
-          <h2 className='font-[600]'><span className='py-[5px] px-[15px] bg-[#FFF0BE] text-center'>Interview</span> <span className='text-[#ff8570]'>Plan</span></h2>
-        </div>
+          <div className='text-[28px] mt-[100px] text-center'>
+            <h2 className='font-[600]'><span className='py-[5px] px-[15px] bg-[#FFF0BE] text-center'>Interview</span> <span className='text-[#ff8570]'>Plan</span></h2>
+          </div>
 
-        <div className='px-[30px] py-[40px] mt-[50px] w-[270px] lg:w-[400px] mx-auto md:px-[50px] md:py-[80px] rounded-[20px] bg-white'>
+          <div className='px-[30px] py-[40px] mt-[50px] w-[270px] lg:w-[400px] mx-auto md:px-[50px] md:py-[80px] rounded-[20px] bg-white'>
             <div>
-            <h3 className='w-[100px] font-[500]'>Invest on yourself just</h3>
-            {/* <img src="" alt="" /> */}
+              <h3 className='w-[100px] font-[500]'>Invest on yourself just</h3>
+              {/* <img src="" alt="" /> */}
             </div>
-            
+
             <h2 className='text-[20px] lg:text-[30px] font-[800]'>₹ 299 to ₹999</h2>
 
             <div className='mt-[20px]'>
               <div className='flex items-center mb-[5px]'>
-              <img className='h-[25px]' src={BulletIcon} alt="bullet icon" />
-              <p className='text-[10px] lg:text-[15px] font-[700] ml-[5px]'>Know how to tackle placements</p>
+                <img className='h-[25px]' src={BulletIcon} alt="bullet icon" />
+                <p className='text-[10px] lg:text-[15px] font-[700] ml-[5px]'>Know how to tackle placements</p>
               </div>
 
               <div className='flex items-center mb-[5px]'>
-              <img className='h-[25px]' src={BulletIcon} alt="bullet icon" />
-              <p className='text-[10px] lg:text-[15px] font-[700] ml-[5px]'>Guidelines to do certifications and projects</p>
+                <img className='h-[25px]' src={BulletIcon} alt="bullet icon" />
+                <p className='text-[10px] lg:text-[15px] font-[700] ml-[5px]'>Guidelines to do certifications and projects</p>
               </div>
 
               <div className='flex items-center'>
-              <img className='h-[25px]' src={BulletIcon} alt="bullet icon" />
-              <p className='text-[10px] lg:text-[15px] font-[700] ml-[5px]'>Guidelines to learn skills</p>
+                <img className='h-[25px]' src={BulletIcon} alt="bullet icon" />
+                <p className='text-[10px] lg:text-[15px] font-[700] ml-[5px]'>Guidelines to learn skills</p>
               </div>
 
             </div>
 
             <div className='text-center'>
-            <button className='px-[10px] py-[8px] md:px-[15px] md:py-[12px] mt-[30px] rounded-[10px] bg-[#ff8570]'>I'm Stepping In</button>
+              <button className='px-[10px] py-[8px] md:px-[15px] md:py-[12px] mt-[30px] rounded-[10px] bg-[#ff8570]'>I'm Stepping In</button>
             </div>
 
+          </div>
         </div>
+
+      </section>
+
+      {/* ====== Mentors cards ====== */}
+      <section className='md:-mt-[30px]'>
+        <h2 className='text-center mb-[50px] text-[28px] font-[800] text-[#E65F46]'>MENTORS</h2>
+        <div className='flex flex-col gap-[50px] md:flex-row md:items-center justify-around'>
+          <div className='relative w-[15rem] h-[20rem] mx-auto md:w-[20rem] md:h-[25rem] rounded-[8px]'>
+            <img src={mentor1} alt="mentors" className='h-full w-full object-cover rounded-[8px]' />
+            <div className='w-full absolute bottom-0 left-0 p-4 bg-[#def2ebbd] rounded-b-[8px]'>
+              <p className='text-[10px] md:text-[20px] font-[700]'>Alexander</p>
+              <p className='text-[#03543C]'>Full Stack Developer</p>
+            </div>
+          </div>
+          <div className='relative w-[15rem] h-[20rem] mx-auto md:w-[20rem] md:h-[25rem] rounded-[8px]'>
+            <img src={mentor2} alt="mentors" className='h-full w-full object-cover rounded-[8px]' />
+            <div className='w-full absolute bottom-0 left-0 p-4 bg-[#def2ebbd] rounded-b-[8px]'>
+              <p className='text-[10px] md:text-[20px] font-[700]'>Michael</p>
+              <p className='text-[#03543C]'>HR Manager, Amazon</p>
+            </div>
+          </div>
+          <div className='relative w-[15rem] h-[20rem] mx-auto md:w-[20rem] md:h-[25rem] rounded-[8px]'>
+            <img src={mentor3} alt="mentors" className='h-full w-full object-cover rounded-[8px]' />
+            <div className='w-full absolute bottom-0 left-0 p-4 bg-[#def2ebbd] rounded-b-[8px]'>
+              <p className='text-[10px] md:text-[20px] font-[700]'>James</p>
+              <p className='text-[#03543C]'>Head HR, VDart</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      {/* ====== users review section ====== */}
+      <section className='reviewSection'>
+        <h2 className='mb-[50px] text-[28px] font-[800] text-[#E65F46] text-center -mt-[30px]'>Reviews</h2>
+
+        <div className='flex flex-col gap-[30px] md:flex-row justify-around'>
+        <div className='m-[20px] md:w-[30rem] md:p-[40px] rounded-[20px] bg-white shadow shadow-black-800'>
+            <p className='p-[20px] font-[500] text-[#4a4a4a]'>ConXpert's virtual interview sessions were incredibly helpful. The expert feedback I received greatly boosted my confidence and prepared me well for my job interviews.</p>
+            <div className='p-[20px] flex items-center gap-[15px]'>
+              <img src={userPic1} alt="profile-image" className='h-[40px] w-[40px] md:h-[60px] md:w-[60px] rounded-full object-cover' />
+              <p className='font-[700]'>Manikandan S</p>
+            </div>
+          </div>
+
+          <div className='m-[20px] md:w-[30rem] md:p-[40px] rounded-[20px] bg-white shadow shadow-black-800'>
+            <p className='p-[20px] font-[500] text-[#4a4a4a]'>I highly recommend ConXpert's services. The personalized attention and guidance from industry experts made all the difference in my interview preparation.</p>
+            <div className='p-[20px] flex items-center gap-[15px]'>
+              <img src={userPic1} alt="profile-image" className='h-[40px] w-[40px] md:h-[60px] md:w-[60px] rounded-full object-cover' />
+              <p className='font-[700]'>Mithun Raj K</p>
+            </div>
+          </div>
+
         </div>
 
       </section>
