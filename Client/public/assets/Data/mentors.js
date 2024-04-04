3bf1ad247dd40b5158a156be83a4a7722d8ddc13
srikishore5727 <srikishore9080676683@gmail.com>
2024-04-04 17:31:46 +0530

    Created a find a mentor page

---
+++ Client/public/assets/Data/mentors.js
@@ -0,0 +1,39 @@
+import mentorImg01 from "../Mentors-images/mentor1.jpeg";
+import mentorImg02 from "../Mentors-images/mentor2.jpg";
+import mentorImg03 from "../Mentors-images/mentor3.jpg";
+
+export const mentors = [
+  {
+    id: "01",
+    name: "Alexander",
+    specialization: "Mock Interview",
+    avgRating: 4.8,
+    totalRating: 75,
+    photo: mentorImg01,
+    totalMentees: 200,
+    position: "Fullstack Developer",
+    company:"Meta"
+  },
+  {
+    id: "02",
+    name: "Michael",
+    specialization: "Mock Interview",
+    avgRating: 4.8,
+    totalRating: 34,
+    photo: mentorImg02,
+    totalMentees:120,
+    position:"HR Manager",
+    company:"Amazon"
+  },
+  {
+    id: "03",
+    name: "James",
+    specialization: "Project Guidance",
+    avgRating: 4.8,
+    totalRating:53,
+    photo: mentorImg03,
+    totalMentees:124,
+    position:"Head HR",
+    company:"VDart"
+  },
+];
